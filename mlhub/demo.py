0cdd54ee6f041b6d7983087f891df8edbebebc97
Graham Williams <Graham.Williams@togaware.com>
2021-04-16 12:07:53 +1000

    Clean up in mlhub style.

--- mlhub/demo.py
+++ mlhub/demo.py
@@ -1,7 +1,30 @@
+# -*- coding: utf-8 -*-
+#
+# Time-stamp: <Friday 2021-04-16 12:06:40 AEST Graham Williams>
+#
+# Copyright (c) Togaware Pty Ltd. All rights reserved.
+# Licensed under the MIT License.
+# Author: Graham.Williams@togaware.com
+#
+# This demo using a pre-built model from
+# https://github.com/zymlnlp/Tweets-Sentiment-Analysis
+
 import torch
 import warnings
+
 from preprocessing import tokenizer
 from nlp_model import SentimentClassifier
+from mlhub.pkg import mlask, mlcat
+
+mlcat("Zeyu Gao's Sentiment Analysis Pre-Built Model", """\
+Welcome to a demo of Zeyu Gao's pre-built model for Sentiment Analysis.
+
+Sentiment analysis is an example application of Natural Language
+Processing (NLP). Sentences are assessed by the model to capture their
+sentiment. See https://onepager.togaware.com.
+
+The model load may take a few seconds.
+""")
 
 warnings.filterwarnings("ignore")
 
@@ -11,42 +34,45 @@ device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 MAX_LEN = 256
 
-if __name__ == '__main__':
+# Load the model.
 
-    # Load the model.
+mfile = "./model/best_model_state.bin"
 
-    try:
-        model = SentimentClassifier(n_classes=3)
-        model.load_state_dict(torch.load("./model/best_model_state.bin",
-                                         map_location=torch.device('cpu')))
-    except:
-        print("No model found, you need to train the model first.")
-        exit()
+try:
+    model = SentimentClassifier(n_classes=3)
+    model.load_state_dict(torch.load(mfile,
+                                     map_location=torch.device('cpu')))
+except Exception:
+    print("No model found. Bad model file or model not yet trained.")
+    print(f"Tried loading '{mfile}'.")
+    exit()
 
-    samples = ["Here's to having a glorious day.",
-               "That was a horrible meal.",
-               "The chef should be sacked.",
-               "Hi there, hope you are well.",
-               "The sun has already risen."]
+mlask()
 
-    for input_text in samples:
+samples = ["Here's to having a glorious day.",
+           "That was a horrible meal.",
+           "The chef should be sacked.",
+           "Hi there, hope you are well.",
+           "The sun has already risen."]
 
-        encoded_tweet = tokenizer.encode_plus(
-            input_text,
-            max_length=MAX_LEN,
-            add_special_tokens=True,
-            return_token_type_ids=False,
-            pad_to_max_length=True,
-            return_attention_mask=True,
-            return_tensors='pt',
-            truncation=True
-        )
+for input_text in samples:
 
-        input_ids = encoded_tweet['input_ids'].to(device)
-        attention_mask = encoded_tweet['attention_mask'].to(device)
+    encoded_tweet = tokenizer.encode_plus(
+        input_text,
+        max_length=MAX_LEN,
+        add_special_tokens=True,
+        return_token_type_ids=False,
+        pad_to_max_length=True,
+        return_attention_mask=True,
+        return_tensors='pt',
+        truncation=True
+    )
 
-        output = model(input_ids, attention_mask)
-        _, prediction = torch.max(output, dim=1)
-
-        print(f'\nSentence  : {input_text}')
-        print(f'Sentiment : {sentiment_map[prediction.numpy()[0]]}')
+    input_ids = encoded_tweet['input_ids'].to(device)
+    attention_mask = encoded_tweet['attention_mask'].to(device)
+    
+    output = model(input_ids, attention_mask)
+    _, prediction = torch.max(output, dim=1)
+    
+    print(f'\nSentence  : {input_text}')
+    print(f'Sentiment : {sentiment_map[prediction.numpy()[0]]}')
