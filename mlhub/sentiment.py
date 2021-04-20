c1d182daf14e89e46ecd19d6b5d9d7c65bd9b27f
Graham Williams <Graham.Williams@togaware.com>
2021-04-20 16:53:05 +1000

    Cleanup and get working.

--- mlhub/sentiment.py
+++ mlhub/sentiment.py
@@ -1,41 +1,89 @@
+# -*- coding: utf-8 -*-
+#
+# Time-stamp: <Tuesday 2021-04-20 16:52:27 AEST Graham Williams>
+#
+# Copyright (c) Togaware Pty Ltd. All rights reserved.
+# Licensed under the MIT License.
+# Author: Graham.Williams@togaware.com
+#
+# This demo using a pre-built model from
+# https://github.com/zymlnlp/Tweets-Sentiment-Analysis
+
+# ----------------------------------------------------------------------
+# Setup
+# ----------------------------------------------------------------------
+
+import os
+import argparse
+
 import torch
-from torch import nn
 import warnings
-from preprocessing import tokenizer
+
 from nlp_model import SentimentClassifier
+from preprocessing import tokenizer
+from mlhub.utils import get_cmd_cwd
+
 warnings.filterwarnings("ignore")
 
-# sentiment_map = {"neutral": 2, "positive": 1, "negative": 0}
-sentiment_map = {2:"neutral", 1:"positive", 0:"negative"}
+# -----------------------------------------------------------------------
+# Process the command line.
+# -----------------------------------------------------------------------
 
+option_parser = argparse.ArgumentParser(add_help=False)
+
+option_parser.add_argument(
+    'sentence',
+    nargs='*',
+    help='sentence to analyse')
+
+option_parser.add_argument(
+    '--file', '-f',
+    help='path to a text file to analyse sentences')
+
+args = option_parser.parse_args()
+
+# ----------------------------------------------------------------------
+# Read the sentences to be analysed.
+# ----------------------------------------------------------------------
+
+text = ""
+if args.file:
+    text = open(os.path.join(get_cmd_cwd(), args.file), "r").read()
+elif args.sentence:
+    text = " ".join(args.sentence)
+
+# Split the text into a list of sentences. Each sentence is sent off
+# for analysis.
+
+text = " ".join(text.splitlines())
+text = " ".join(text.splitlines())
+text = text.replace(". ", "\n")
+text = text.splitlines()
+
+sentiment_map = {2: "neutral", 1: "positive", 0: "negative"}
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
 MAX_LEN = 256
 
+# Load the model.
 
-if __name__ == '__main__':
+mfile = "./model/best_model_state.bin"
 
-    # load in the model
-    try:
-        model = SentimentClassifier(n_classes=3)
-        model.load_state_dict(torch.load("./model/best_model_state.bin", map_location=torch.device('cpu')))
-    except:
-        print("No model found, you need to train the model first.")
-        exit()
+try:
+    model = SentimentClassifier(n_classes=3)
+    model.load_state_dict(torch.load(mfile,
+                                     map_location=torch.device('cpu')))
+except Exception:
+    print("No model found. Bad model file or model not yet trained.")
+    print(f"Tried loading '{mfile}'.")
+    exit()
 
-
-    while True:
-        input_text = str(input("Please input tweet text:"))
-
-        if input_text == "exit program":
-            break
-
-        if len(input_text) == 0:
-            print("No input text detected, will use sample text: 'I like this movie.' ")
-            input_text = "I like this movie."
+if len(text):
+    for sentence in text:
 
         encoded_tweet = tokenizer.encode_plus(
-            input_text,
+            sentence,
             max_length=MAX_LEN,
             add_special_tokens=True,
             return_token_type_ids=False,
@@ -51,41 +99,4 @@ if __name__ == '__main__':
         output = model(input_ids, attention_mask)
         _, prediction = torch.max(output, dim=1)
 
-        print(f'Tweet text: {input_text}')
-        print(f'Sentiment  : {sentiment_map[prediction.numpy()[0]]} \n')
-
-
-def get_predictions(model, data_loader):
-
-    model = model.eval()
-
-    tweet_texts = []
-    predictions = []
-    prediction_probs = []
-    real_values = []
-
-    with torch.no_grad():
-        for d in data_loader:
-
-            texts = d["tweet_text"]
-            input_ids = d["input_ids"].to(device)
-            attention_mask = d["attention_mask"].to(device)
-            targets = d["targets"].to(device)
-
-            outputs = model(
-                input_ids=input_ids,
-                attention_mask=attention_mask
-            )
-            _, preds = torch.max(outputs, dim=1)
-
-            probs = nn.functional.softmax(outputs, dim=1)
-
-            tweet_texts.extend(texts)
-            predictions.extend(preds)
-            prediction_probs.extend(probs)
-            real_values.extend(targets)
-
-    predictions = torch.stack(predictions).cpu()
-    prediction_probs = torch.stack(prediction_probs).cpu()
-    real_values = torch.stack(real_values).cpu()
-    return tweet_texts, predictions, prediction_probs, real_values
+        print(f'"{sentence}",{sentiment_map[prediction.numpy()[0]]}')
